abb85e5e4df152d54bbe6df3eb5a4e150ed3b54a
Quraatulain <anniekhan549@gmail.com>
2018-07-18 23:13:46 +0500

    game

---
+++ tic tacc/tic tacc/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace tic_tacc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A00 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A01 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.Ngbutton = new System.Windows.Forms.Button();
+            this.rbutton = new System.Windows.Forms.Button();
+            this.EBUTTON = new System.Windows.Forms.Button();
+            this.XWin = new System.Windows.Forms.Label();
+            this.OWin = new System.Windows.Forms.Label();
+            this.Draws = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // A00
+            // 
+            this.A00.Location = new System.Drawing.Point(35, 41);
+            this.A00.Name = "A00";
+            this.A00.Size = new System.Drawing.Size(77, 58);
+            this.A00.TabIndex = 0;
+            this.A00.UseVisualStyleBackColor = true;
+            this.A00.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A10
+            // 
+            this.A10.Location = new System.Drawing.Point(35, 105);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(77, 58);
+            this.A10.TabIndex = 1;
+            this.A10.UseVisualStyleBackColor = true;
+            this.A10.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A20
+            // 
+            this.A20.Location = new System.Drawing.Point(35, 169);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(77, 58);
+            this.A20.TabIndex = 2;
+            this.A20.UseVisualStyleBackColor = true;
+            this.A20.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A01
+            // 
+            this.A01.Location = new System.Drawing.Point(118, 41);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(77, 58);
+            this.A01.TabIndex = 3;
+            this.A01.UseVisualStyleBackColor = true;
+            this.A01.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A11
+            // 
+            this.A11.Location = new System.Drawing.Point(118, 105);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(77, 58);
+            this.A11.TabIndex = 4;
+            this.A11.UseVisualStyleBackColor = true;
+            this.A11.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A21
+            // 
+            this.A21.Location = new System.Drawing.Point(118, 169);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(77, 58);
+            this.A21.TabIndex = 5;
+            this.A21.UseVisualStyleBackColor = true;
+            this.A21.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A22
+            // 
+            this.A22.Location = new System.Drawing.Point(201, 169);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(77, 58);
+            this.A22.TabIndex = 8;
+            this.A22.UseVisualStyleBackColor = true;
+            this.A22.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A12
+            // 
+            this.A12.Location = new System.Drawing.Point(201, 105);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(77, 58);
+            this.A12.TabIndex = 7;
+            this.A12.UseVisualStyleBackColor = true;
+            this.A12.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A02
+            // 
+            this.A02.Location = new System.Drawing.Point(201, 41);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(77, 58);
+            this.A02.TabIndex = 6;
+            this.A02.UseVisualStyleBackColor = true;
+            this.A02.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Ngbutton
+            // 
+            this.Ngbutton.Location = new System.Drawing.Point(35, 273);
+            this.Ngbutton.Name = "Ngbutton";
+            this.Ngbutton.Size = new System.Drawing.Size(113, 37);
+            this.Ngbutton.TabIndex = 9;
+            this.Ngbutton.Text = "New Game";
+            this.Ngbutton.UseVisualStyleBackColor = true;
+            this.Ngbutton.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // rbutton
+            // 
+            this.rbutton.Location = new System.Drawing.Point(163, 273);
+            this.rbutton.Name = "rbutton";
+            this.rbutton.Size = new System.Drawing.Size(115, 37);
+            this.rbutton.TabIndex = 10;
+            this.rbutton.Text = "Reset";
+            this.rbutton.UseVisualStyleBackColor = true;
+            this.rbutton.Click += new System.EventHandler(this.rbutton_Click);
+            // 
+            // EBUTTON
+            // 
+            this.EBUTTON.Location = new System.Drawing.Point(338, 278);
+            this.EBUTTON.Name = "EBUTTON";
+            this.EBUTTON.Size = new System.Drawing.Size(107, 32);
+            this.EBUTTON.TabIndex = 11;
+            this.EBUTTON.Text = "Exit";
+            this.EBUTTON.UseVisualStyleBackColor = true;
+            this.EBUTTON.Click += new System.EventHandler(this.button10_Click_1);
+            // 
+            // XWin
+            // 
+            this.XWin.Location = new System.Drawing.Point(335, 53);
+            this.XWin.Name = "XWin";
+            this.XWin.Size = new System.Drawing.Size(92, 19);
+            this.XWin.TabIndex = 12;
+            this.XWin.Text = "label1";
+            this.XWin.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // OWin
+            // 
+            this.OWin.Location = new System.Drawing.Point(335, 105);
+            this.OWin.Name = "OWin";
+            this.OWin.Size = new System.Drawing.Size(92, 19);
+            this.OWin.TabIndex = 13;
+            this.OWin.Text = "label2";
+            this.OWin.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Draws
+            // 
+            this.Draws.Location = new System.Drawing.Point(335, 169);
+            this.Draws.Name = "Draws";
+            this.Draws.Size = new System.Drawing.Size(92, 19);
+            this.Draws.TabIndex = 14;
+            this.Draws.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 342);
+            this.Controls.Add(this.Draws);
+            this.Controls.Add(this.OWin);
+            this.Controls.Add(this.XWin);
+            this.Controls.Add(this.EBUTTON);
+            this.Controls.Add(this.rbutton);
+            this.Controls.Add(this.Ngbutton);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A01);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A00;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button Ngbutton;
+        private System.Windows.Forms.Button rbutton;
+        private System.Windows.Forms.Button EBUTTON;
+        private System.Windows.Forms.Label XWin;
+        private System.Windows.Forms.Label OWin;
+        private System.Windows.Forms.Label Draws;
+    }
+}
+
---
+++ tic tacc/tic tacc/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tacc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int player = 2;// even x turn;odd=0 turn;
+        public int turns = 0;//counting turns;
+        public int s1 = 0;
+        public int s2 = 0;
+        public int sd = 0;
+        //counting wins for both players and draws;
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            NewGame();
+
+        }
+        bool checkdraw()
+        {
+
+            if ((turns == 9) && checkwinner() == false)
+                return true;
+            else
+                return false;
+        }
+        bool checkwinner()
+        {
+            //horizontal checks
+            if((A00.Text==A01.Text) &&  (A01.Text == A02.Text) && A00.Text !="")
+            return true;
+            else if((A10.Text==A11.Text) && (A11.Text==A12.Text) && A10.Text !="")
+                return true;
+          else if ((A20.Text==A21.Text) && (A21.Text==A22.Text) && A20.Text !="")
+return true;
+            //vertical checks
+            if((A00.Text==A10.Text) && (A10.Text==A20.Text) && A00.Text!="")
+            return true;
+             if((A01.Text==A11.Text) && (A11.Text==A21.Text) && A01.Text !="")
+            return true;
+        else if((A02.Text==A12.Text) && (A12.Text==A22.Text) && A02.Text !="")
+            return true;
+            // diagonal checks
+            if((A00.Text==A11.Text) && (A11.Text==A22.Text) && A00.Text !="")
+                return true;
+            else if((A01.Text==A11.Text) && (A11.Text==A21.Text) && A01.Text !="")
+                return true;
+            else
+                return false;
+ 
+    }
+
+        private void button10_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Text == "")
+            {
+                if (player % 2 == 0)
+                {
+                    button.Text = "x";
+                    player++;
+                    turns++;
+                }
+                else
+                {
+                    button.Text = "0";
+                    player++;
+                    turns++;
+
+                }
+    if (checkdraw()==true)
+{
+   MessageBox.Show("tie game");
+    sd ++;
+    NewGame();
+            }
+            }
+                if (checkwinner()==true)   
+                {
+                    if(button.Text=="x")
+                    {
+                        MessageBox.Show("X Won!");
+                        s1++;
+                        NewGame();
+                    }
+                    else
+                    {
+                        MessageBox.Show("0 Win!");
+                        s2++;
+                        NewGame();
+                    }
+                            
+                            
+                }
+}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "TIC TAC TOEE";
+
+            XWin.Text = "X:"+s1;
+            OWin.Text = "0:" + s2;
+            Draws.Text="draws:"+sd;
+        }
+            void NewGame()
+            {
+                player = 2;
+                turns = 0;
+              A00.Text= A11.Text = A02.Text = A10.Text = A11.Text = A12.Text = A20.Text = A21.Text = A22.Text = "";
+                XWin.Text = "X:"+s1;
+            OWin.Text = "o:" + s2;
+            Draws.Text="draws:"+sd;
+                
+            }
+
+        private void rbutton_Click(object sender, EventArgs e)
+        {
+            s1=s2=sd=0;
+            NewGame();
+        }
+
+
+
+
+
+
+        }
+}
